2f08e8815a9e370ae8e0906a8ef72c957fe486c4
Garin87 <kamrader@gmail.com>
2018-09-29 15:18:19 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -5,36 +5,43 @@ module.exports = function solveSudoku(matrix) {
     
     if(mainSudoku(matrix, 9,9)) return matrix;
 }
-
-function noConflicts(matrix, row, col, num) {
-    return isRowOk(matrix, row, num) && isColOk(matrix, col, num) && isBoxOk(matrix, row, col, num);
+function findUnassignedLocation(matrix, row, col) {
+    for (row = 0; row < 9 ; row++)
+        for (col =0; col < 9 ; col++)
+            if (matrix[row][col] == 0)
+                return [row, col];
+    return [-1, -1];
 }
 
-function isRowOk(matrix, row, num) {
+function used_in_row(matrix, row, number) {
     for (var col = 0; col < 9; col++)
-        if (matrix[row][col] == num)
+        if (matrix[row][col] == number)
             return false;
 
     return true;
 }
 
-function isColOk(matrix, col, num) {
+function used_in_col(matrix, col, number) {
     for (var row = 0; row < 9; row++)
-    if (matrix[row][col] == num)
+    if (matrix[row][col] == number)
         return false;
 
     return true;    
 }
 
-function isBoxOk(matrix, row, col, num) {
+function used_in_box(matrix, row, col, number) {
     row = Math.floor(row / 3) * 3;
     col = Math.floor(col / 3) * 3;
 
     for (var r = 0; r < 3; r++)
         for (var c = 0; c < 3; c++)
-            if (matrix[row + r][col + c] == num)
+            if (matrix[row + r][col + c] == number)
                 return false;
     return true;
+}
+
+function is_safe(matrix, row, col, number) {
+    return used_in_row(matrix, row, number) && used_in_col(matrix, col, number) && used_in_box(matrix, row, col, number);
 }
 
 function mainSudoku(matrix, row, col) {
@@ -44,10 +51,10 @@ function mainSudoku(matrix, row, col) {
 
     if (row == -1) return true;
     
-    for (var num = 1; num <= 9; num++) {
+    for (var number = 1; number <= 9; number++) {
 
-        if ( noConflicts(matrix, cell[0], cell[1], num) ) {   
-            matrix[row][col] = num;
+        if ( is_safe(matrix, cell[0], cell[1], number) ) {   
+            matrix[row][col] = number;
 
             if ( mainSudoku(matrix, 9, 9) ) {                
                 return true;
@@ -59,13 +66,7 @@ function mainSudoku(matrix, row, col) {
 }
 
 
-function findUnassignedLocation(matrix, row, col) {
-    for (row = 0; row < 9 ; row++)
-        for (col =0; col < 9 ; col++)
-            if (matrix[row][col] == 0)
-                return [row, col];
-    return [-1, -1];
-}
+
 
 
 
