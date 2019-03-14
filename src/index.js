ec21af61ed4f8e2530063b6efd3c609581d91007
Stas ilyukovich <32239229+garin87@users.noreply.github.com>
2019-03-14 16:43:13 +0200

    change variables var to let

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function findUnassignedLocation(matrix, row, col) {
 }
 
 function used_in_row(matrix, row, number) {
-    for (var col = 0; col < 9; col++)
+    for (let col = 0; col < 9; col++)
         if (matrix[row][col] == number)
             return false;
 
@@ -22,7 +22,7 @@ function used_in_row(matrix, row, number) {
 }
 
 function used_in_col(matrix, col, number) {
-    for (var row = 0; row < 9; row++)
+    for (let row = 0; row < 9; row++)
     if (matrix[row][col] == number)
         return false;
 
@@ -33,8 +33,8 @@ function used_in_box(matrix, row, col, number) {
     row = Math.floor(row / 3) * 3;
     col = Math.floor(col / 3) * 3;
 
-    for (var r = 0; r < 3; r++)
-        for (var c = 0; c < 3; c++)
+    for (let r = 0; r < 3; r++)
+        for (let c = 0; c < 3; c++)
             if (matrix[row + r][col + c] == number)
                 return false;
     return true;
@@ -45,13 +45,13 @@ function is_safe(matrix, row, col, number) {
 }
 
 function mainSudoku(matrix, row, col) {
-    var index = findUnassignedLocation(matrix, row, col);
+    let index = findUnassignedLocation(matrix, row, col);
     row = index[0];
     col = index[1];
 
     if (row == -1) return true;
     
-    for (var number = 1; number <= 9; number++) {
+    for (let number = 1; number <= 9; number++) {
 
         if ( is_safe(matrix, index[0], index[1], number) ) {   
             matrix[row][col] = number;
